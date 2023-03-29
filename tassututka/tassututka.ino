d00f8c3bda09176366fc3db0539201c3f76c1393
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2023-03-29 19:59:44 +0300

    Let's start here :)

--- tassututka/tassututka.ino
+++ tassututka/tassututka.ino
@@ -8,29 +8,53 @@
 /****************** REBECCA SOISENNIEMI***/
 
 #include <SoftwareSerial.h>
+SoftwareSerial GSMSerial(7, 8);
+int Powerkey = 9;
+void setup()
+{
+pinMode(Powerkey, OUTPUT);   // initialize the digital pin as an output.
+virratPaalle();
+GSMSerial.begin(19200);               // the GPRS/GSM baud rate
+Serial.begin(9600);                 // the GPRS/GSM baud rate
+gpsPaalle();
 
-SoftwareSerial gpsSerial(10, 11); // GPS-moduulin RX ja TX -pinnit
-String gpsData;
+// AT+CGNSPWR=1
+// AT+CGNSINF
+// AT+CGPSSTATUS?
+// AT+CGPSOUT=32
+// AT+CGPSRST=0
+// AT+CGNSTST=1
+// AT+CCLK?
+// AT+CGNSPWR=1;+CGNSCMD=0,"$PMTK314,8,8,8,8,8,8,0,0,0,0,0,0,0,0,0,0,0*28";+CGNSTST=1;
+// AT+CFUN=0 tai 1 tai 4 0=minimum functionality 1=full functionality defaul 4 = flightmode disable RF
 
-void setup() {
-  Serial.begin(9600); // Sarjaportti tietokoneelle
-  gpsSerial.begin(9600); // Sarjaportti GPS-moduulille
 }
 
-void loop() {
-  gpsSerial.println("AT+CGPSPWR=1"); // Laita GPS-moduuli päälle
-  delay(1000);
-  gpsSerial.println("AT+CGPSSTATUS?"); // Tarkista GPS-moduulin tila
-  delay(1000);
-  while (gpsSerial.available() > 0) {
-    gpsData = gpsSerial.readString();
-    Serial.println(gpsData);
-  }
-  gpsSerial.println("AT+CGPSINF=0"); // Hae GPS-sijaintitiedot
-  delay(1000);
-  while (gpsSerial.available() > 0) {
-    gpsData = gpsSerial.readString();
-    Serial.println(gpsData);
-  }
-  delay(5000);
-  
+void loop()
+{
+if(Serial.available())
+
+GSMSerial.print((char)Serial.read());
+
+else  if(GSMSerial.available())
+
+Serial.print((char)GSMSerial.read());
+
+}
+
+void virratPaalle(void)
+{
+  digitalWrite(Powerkey, LOW); 
+  delay(500);               // odottaa hetke
+  digitalWrite(Powerkey, HIGH);
+}
+
+void gpsPaalle()
+{
+delay(1000);
+GSMSerial.println("AT");
+delay(4000);
+GSMSerial.println("AT+CFUN=0");
+delay(1000);
+GSMSerial.println("AT+CGNSPWR=1");
+}
