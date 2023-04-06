e3eb90e9f86ce51aa60b80de17041928bbebf3a6
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2023-04-06 10:52:20 +0300

    sisennyksii yms pientä

--- tassututka/tassututka.ino
+++ tassututka/tassututka.ino
@@ -1,60 +1,79 @@
-/************* TASSUTUTKA ****************/
-/**** TIETOTEKNIIKAN SOVELLUSPROJEKTI ****/
-/********************************* OAMK **/
-/**** ARDUINOKOODIT **********************/
-/****************** PASI PUHAKKA *********/
-/****************** MIKKO KUJALA *********/
-/****************** NICO HERTOLIN ********/
-/****************** REBECCA SOISENNIEMI***/
+/***************************************************************************************/
+/**** TASSUTUTKA ***********************************************************************/
+/**** TIETOTEKNIIKAN SOVELLUSPROJEKTI, OULUN AMMATTIKORKEAKOULU, TVT23KMO **************/
+/**** TEKIJÄT : MIKKO KUJALA / PASI PUHAKKA / REBECCA SOISENNIEMI / NICO HERTOLIN ******/
+/***************************************************************************************/
 
 #include <SoftwareSerial.h>
 SoftwareSerial GSMSerial(7, 8);
 int Powerkey = 9;
+
 void setup()
 {
-pinMode(Powerkey, OUTPUT);   // initialize the digital pin as an output.
-virratPaalle();
-GSMSerial.begin(19200);               // the GPRS/GSM baud rate
-Serial.begin(9600);                 // the GPRS/GSM baud rate
-gpsPaalle();
+  pinMode(Powerkey, OUTPUT);    // initialize the digital pin as an output.
+  virratPaalle();
+  GSMSerial.begin(19200);       // the GPRS/GSM baud rate
+  Serial.begin(9600);           // the GPRS/GSM baud rate
+  gpsPaalle();
 
-// AT+CGNSPWR=1
-// AT+CGNSINF
-// AT+CGPSSTATUS?
-// AT+CGPSOUT=32
-// AT+CGPSRST=0
-// AT+CGNSTST=1
-// AT+CCLK?
-// AT+CGNSPWR=1;+CGNSCMD=0,"$PMTK314,8,8,8,8,8,8,0,0,0,0,0,0,0,0,0,0,0*28";+CGNSTST=1;
-// AT+CFUN=0 tai 1 tai 4 0=minimum functionality 1=full functionality defaul 4 = flightmode disable RF
+              /* 
+              SIM900.begin(19200);  //SIM900 serial communication is starting whit 19200 of baudrate speed
+              AT+CGNSPWR=1
+              AT+CGNSINF
+              AT+CGPSSTATUS?
+              AT+CGPSOUT=32
+              AT+CGPSRST=0
+              AT+CGNSTST=1
+              AT+CCLK?
+              AT+CGNSPWR=1;+CGNSCMD=0,"$PMTK314,8,8,8,8,8,8,0,0,0,0,0,0,0,0,0,0,0*28";+CGNSTST=1;
+              AT+CFUN=0 tai 1 tai 4 0=minimum functionality 1=full functionality defaul 4 = flightmode disable RF
 
+              void sendSMS() {
+
+              // AT command to set SIM900 to SMS mode
+              SIM900.print("AT+CMGF=1\r"); 
+              delay(100);
+
+              // REPLACE THE X's WITH THE RECIPIENT'S MOBILE NUMBER
+              // USE INTERNATIONAL FORMAT CODE FOR MOBILE NUMBERS
+              SIM900.println("AT+CMGS=\"+358443286650\""); 
+              delay(100);
+
+              // REPLACE WITH YOUR OWN SMS MESSAGE CONTENT
+              SIM900.println("Tutka havaitsi kohteen."); 
+              delay(100);
+
+              // End AT command with a ^Z, ASCII code 26
+              SIM900.println((char)26); 
+              delay(100);
+              SIM900.println();
+              // Give module time to send SMS
+              delay(5000); 
+              }
+              */
 }
 
 void loop()
 {
-if(Serial.available())
-
-GSMSerial.print((char)Serial.read());
-
-else  if(GSMSerial.available())
-
-Serial.print((char)GSMSerial.read());
-
+  if(Serial.available())
+  GSMSerial.print((char)Serial.read());
+  else if(GSMSerial.available())
+  Serial.print((char)GSMSerial.read());
 }
 
 void virratPaalle(void)
 {
-  digitalWrite(Powerkey, LOW); 
-  delay(500);               // odottaa hetke
+  digitalWrite(Powerkey, LOW);
+  delay(500);
   digitalWrite(Powerkey, HIGH);
 }
 
 void gpsPaalle()
 {
-delay(1000);
-GSMSerial.println("AT");
-delay(4000);
-GSMSerial.println("AT+CFUN=0");
-delay(1000);
-GSMSerial.println("AT+CGNSPWR=1");
+  delay(1000);
+  GSMSerial.println("AT");
+  delay(4000);
+  GSMSerial.println("AT+CFUN=0");
+  delay(1000);
+  GSMSerial.println("AT+CGNSPWR=1");
 }
