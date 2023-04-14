043d03e7ddd2015804e5df0ae4a113e1c8502de3
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2023-04-14 22:24:30 +0300

    Few changes in .h, .cpp, .ino
    Check these out

--- tassututka/tassudata.h
+++ tassututka/tassudata.h
@@ -3,6 +3,10 @@
 #ifndef TASSUDATA_SEND_H
 #define TASSUDATA_SEND_H
 
+// AT Command Helper 
+// sends command into the serial and waits for the response
+int atCommandHelper(SoftwareSerial & swSerial, char* ATcommand, char* ATresponse1, char* ATresponse2, unsigned long timeout);
+
 // Connect to server
 bool connect(SoftwareSerial & swSerial, char* ipAddress);
 
